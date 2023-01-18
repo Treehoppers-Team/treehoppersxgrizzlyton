9cbb15d8e8b598511d88b9c13bc5cbf7e5ce4e08
advait <advait.deshpande@live.com>
2023-01-18 18:47:25 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/Treehoppers-Team/mynt-app""
    This reverts commit dcf8c1a72c6023dc9eb08dd4273a14b924b52198.

---
+++ app/telegram/server/server.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkhEEbHzLsVNnl6iVJtXegV9zuljKB5i8",
+  authDomain: "treehoppers-mynt.firebaseapp.com",
+  projectId: "treehoppers-mynt",
+  storageBucket: "treehoppers-mynt.appspot.com",
+  messagingSenderId: "751257459683",
+  appId: "1:751257459683:web:10c7f44cd9684098205ed6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- frontend/telegram/test.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("This is a basic python script")
-
-if __name__ == "__main__":
-    main()
