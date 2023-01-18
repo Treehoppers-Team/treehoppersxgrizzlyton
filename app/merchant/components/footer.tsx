3c9fd2f8cadac7d9962bbf6055ee2cdd46f22dcd
advait <advait.deshpande@live.com>
2023-01-18 23:10:10 +0800

    changed folder structure

--- app/merchant/components/footer.tsx
+++ app/merchant/components/footer.tsx
@@ -2,9 +2,10 @@ export default function Footer() {
   return (
     <footer className="bg-white text-center lg:text-left">
       <div className="text-gray-700 text-center p-4">
-        © 2022 Copyright:
+        © 2022 Copyright:  
+        <a> </a>
         <a className="text-gray-800" href="https://tailwind-elements.com/">
-          Treehoppers
+           Treehoppers
         </a>
       </div>
     </footer>
