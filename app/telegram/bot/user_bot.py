c2bcfc3b64bcb6592d1b6fd168573344f7ee39f6
rilwan99 <rilwanwild@gmail.com>
2023-02-15 18:19:11 +0800

    include error handler

--- app/telegram/bot/user_bot.py
+++ app/telegram/bot/user_bot.py
@@ -58,6 +58,25 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=update.effective_chat.id,
         text="Sorry, I didn't understand that command."
     )
+    
+    
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = "".join(tb_list)
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f"An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+    await context.bot.send_message(
+        chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML
+    )
+    return ConversationHandler.END
 
 
 """"
@@ -557,5 +576,6 @@ if __name__ == '__main__':
     application.add_handler(PreCheckoutQueryHandler(precheckout))
     application.add_handler(MessageHandler(
         filters.SUCCESSFUL_PAYMENT, successful_payment))
+    application.add_error_handler(error_handler)
 
     application.run_polling()
