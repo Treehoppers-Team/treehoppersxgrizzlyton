9fe965c9e38f12b68290a428b5f938fddabb1825
AvranWong <avranwong@gmail.com>
2023-03-06 22:50:46 +0700

    Small front end change, add a button to the events page to select participants

--- app/merchant/components/event.tsx
+++ app/merchant/components/event.tsx
@@ -157,6 +157,26 @@ export default function Card({
                 Conduct Raffle
               </button>
               <button
+                onClick={onOpenRaffleModal}
+                className="flex flex-wrap items-center mx-1 text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-2.5 text-center"
+              >
+                {/* <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                  />
+                </svg>{" "} */}
+                Select Participants
+              </button>
+              <button
                 onClick={onOpenEditModal}
                 className="mx-1 text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-2.5 text-center"
               >
